92a7dcb5d16c04f3299ee6bd9229884dffa6a4c4
Jack brookes <jbrookes187@gmail.com>
2022-07-22 00:10:30 +0100

    tabs to spaces

--- Assets/Scripts/FirstPersonMovementController.cs
+++ Assets/Scripts/FirstPersonMovementController.cs
@@ -8,55 +8,55 @@ using UnityEngine;
 /// </summary>
 public class FirstPersonMovementController : MonoBehaviour
 {
-	private const int RIGHT_MOUSE_BUTTON = 1; 
+    private const int RIGHT_MOUSE_BUTTON = 1; 
 
-	[Header("Looking")]
-	[SerializeField] private float _sensitivity = 5.0f;
+    [Header("Looking")]
+    [SerializeField] private float _sensitivity = 5.0f;
 
-	[Header("Movement")]
-	[SerializeField] private float _speed = 5.0f;
+    [Header("Movement")]
+    [SerializeField] private float _speed = 5.0f;
 
-	private Vector3 _lookEulers = Vector3.zero;
+    private Vector3 _lookEulers = Vector3.zero;
 
-	private void Update()
-	{
-		// lock cursor while left mouse button held
-		if (Input.GetMouseButton(RIGHT_MOUSE_BUTTON))
-		{
-			Cursor.lockState = CursorLockMode.Locked;
+    private void Update()
+    {
+        // lock cursor while left mouse button held
+        if (Input.GetMouseButton(RIGHT_MOUSE_BUTTON))
+        {
+            Cursor.lockState = CursorLockMode.Locked;
 
-			// only allow looking whilst left mouse button is held
-			UpdateLook();
-		}
-		else
-		{
-			Cursor.lockState = CursorLockMode.None;
-		}
+            // only allow looking whilst left mouse button is held
+            UpdateLook();
+        }
+        else
+        {
+            Cursor.lockState = CursorLockMode.None;
+        }
 
-		UpdateMovement();
-	}
+        UpdateMovement();
+    }
 
 
-	private void UpdateLook()
-	{
-		Vector3 mouseDelta = new Vector3(-Input.GetAxisRaw("Mouse Y"), Input.GetAxisRaw("Mouse X"), 0);
-		mouseDelta *= _sensitivity;
+    private void UpdateLook()
+    {
+        Vector3 mouseDelta = new Vector3(-Input.GetAxisRaw("Mouse Y"), Input.GetAxisRaw("Mouse X"), 0);
+        mouseDelta *= _sensitivity;
 
-		// incrementally add to the camera look
-		_lookEulers += mouseDelta * Time.deltaTime;
+        // incrementally add to the camera look
+        _lookEulers += mouseDelta * Time.deltaTime;
 
-		// prevent looking all the way up or down
-		_lookEulers.y = Mathf.Clamp(_lookEulers.y, -90, 90);
+        // prevent looking all the way up or down
+        _lookEulers.y = Mathf.Clamp(_lookEulers.y, -90, 90);
 
-		transform.rotation = Quaternion.Euler(_lookEulers);
-	}
+        transform.rotation = Quaternion.Euler(_lookEulers);
+    }
 
-	private void UpdateMovement()
-	{
-		float forward = Input.GetAxis("Vertical");
-		float strafe = Input.GetAxis("Horizontal");
+    private void UpdateMovement()
+    {
+        float forward = Input.GetAxis("Vertical");
+        float strafe = Input.GetAxis("Horizontal");
 
-		Vector3 movement = new Vector3(strafe, 0, forward) * _speed * Time.deltaTime;
-		transform.position += transform.TransformVector(movement);
-	}
+        Vector3 movement = new Vector3(strafe, 0, forward) * _speed * Time.deltaTime;
+        transform.position += transform.TransformVector(movement);
+    }
 }
--- Assets/Scripts/GameConstants.cs
+++ Assets/Scripts/GameConstants.cs
@@ -1,9 +1,9 @@
 public static class GameConstants
 {
-	public const int DEFAULT_SIZE = 20;
-	public const float DEFAULT_TIMESTEP = 0.1f;
-	public const int DEFAULT_UNDERPOPULATION_THRESHOLD = 7;
-	public const int DEFAULT_REPRODUCTION_CRITERION = 10;
-	public const int DEFAULT_OVERPOPULATION_THRESHOLD = 12;
-	public const float DEFAULT_SPAWN_RATE = 0.35f;
+    public const int DEFAULT_SIZE = 20;
+    public const float DEFAULT_TIMESTEP = 0.1f;
+    public const int DEFAULT_UNDERPOPULATION_THRESHOLD = 7;
+    public const int DEFAULT_REPRODUCTION_CRITERION = 10;
+    public const int DEFAULT_OVERPOPULATION_THRESHOLD = 12;
+    public const float DEFAULT_SPAWN_RATE = 0.35f;
 }
